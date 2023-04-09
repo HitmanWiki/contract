17d501ea3af7796b4881b75b67c274e96c4be120
HitmanWiki <abhimanyumalhotra.2021@gmail.com>
2023-04-09 18:00:12 +0530

    first3 commit

--- config-telegram.bot.js
+++ config-telegram.bot.js
@@ -1,4 +1,4 @@
 const config = require ("./config.js")
 const TelegramBot = require('node-telegram-bot-api');
-const bot = new TelegramBot(config.TELEGRAM_BOT_TOKEN, { polling: Boolean(config.TELEGRAM_POLLING_ON ?? false) });
+const bot = new TelegramBot(config.TELEGRAM_BOT_TOKEN, { polling: Boolean(config.TELEGRAM_POLLING_ON ?? true) });
 module.exports = { bot };
